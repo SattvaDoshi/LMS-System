fc6e00533cead9db94fb4337e54e4c4c01b43723
SattvaDoshi <sattva103@gmail.com>
2024-06-02 13:17:37 +0530

    Deployment

--- client/src/Helpers/axiosInstance.js
+++ client/src/Helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:5014/api/v1";
+const BASE_URL = "https://lms-system-backend-9sor.onrender.com/api/v1";
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
